a121cdfc327f8b44ffe45f832c5244d879bf7253
Guillaume <Guillaume>
2023-08-08 16:47:59 +0200

    Parsing now returns a KeyValue struct

--- vdf.go
+++ vdf.go
@@ -3,9 +3,9 @@ package vdf
 import (
 	"github.com/golang-collections/collections/stack"
 	"unicode/utf8"
+	"fmt"
+	"encoding/json"
 )
-
-var depth = 0
 
 type Token int
 
@@ -23,16 +23,214 @@ type VDF struct {
 	len int
 }
 
+type KeyValue struct {
+	key string
+	value interface{}
+	isRoot bool `default:false`
+}
 
-func (this *VDF) Parse(s []byte) map[string]interface{} {
+func PrintTabs(tabs int) {
+	for i := 0; i < tabs; i++ {
+		fmt.Print("\t")
+	}
+}
+
+func (this KeyValue) GetString(key string) (string, bool) {
+	a, ok := this.Get(key)
+	if ok {
+		switch a.value.(type) {
+		case string:
+			return a.value.(string), true
+		}
+	}
+	return "", false
+}
+
+func (this KeyValue) ToString() (string, bool) {
+	switch this.value.(type) {
+	case string:
+		return this.value.(string), true
+	}
+	return "", false
+}
+
+func (this KeyValue) Get(key string) (*KeyValue, bool) {
+	switch this.value.(type) {
+	case string:
+		return nil, false
+	case []*KeyValue:
+		arr := this.value.([]*KeyValue)
+		for _, item := range arr {
+			if key == item.key {
+				return item, true
+			}
+		}
+	}
+	return nil, false
+}
+
+func (this KeyValue) GetAll(key string) ([]*KeyValue, bool) {
+	switch this.value.(type) {
+	case string:
+		return nil, false
+	case []*KeyValue:
+		ret := []*KeyValue{}
+		arr := this.value.([]*KeyValue)
+		for _, item := range arr {
+			if key == item.key {
+				ret = append(ret, item)
+			}
+		}
+		if len(ret) > 0 {
+			return ret, true
+		}
+	}
+	return nil, false
+}
+
+func (this KeyValue) GetSubElement(path []string) (*KeyValue, bool) {
+	if subElements, ok := this.GetAll(path[0]); ok {
+		if len(path) == 1 {
+			return subElements[0], true
+		} else {
+			for _, subElement := range subElements {
+				if subElement2, ok := subElement.GetSubElement(path[1:]); ok {
+					return subElement2, true
+				}
+			}
+		}
+	}
+	return nil, false
+}
+
+func (this KeyValue) GetChilds() ([]*KeyValue) {
+	switch this.value.(type) {
+	case []*KeyValue:
+		return this.value.([]*KeyValue)
+	}
+	return []*KeyValue{}
+}
+
+func (this KeyValue) ToStringMap() (*map[string]string, bool) {
+	switch this.value.(type) {
+	case []*KeyValue:
+		ret := make(map[string]string)
+		arr := this.value.([]*KeyValue)
+		for _, item := range arr {
+			switch item.value.(type) {
+			case string:
+				ret[item.key] = item.value.(string)
+			}
+		}
+		return &ret, true
+	}
+	return nil, false
+}
+
+func (this KeyValue) GetStringMap(key string) (*map[string]string, bool) {
+	if sub, ok := this.Get(key); ok {
+		return sub.ToStringMap()
+	}
+	return nil, false
+}
+
+func (this KeyValue) GetSubElementStringMap(path []string) (*map[string]string, bool) {
+	if sub, ok := this.GetSubElement(path); ok {
+		return sub.ToStringMap()
+	}
+	return nil, false
+}
+
+func (this KeyValue) RemoveDuplicates() {
+	switch this.value.(type) {
+	case []*KeyValue:
+		allKeys := make(map[string]bool)
+		list := []*KeyValue{}
+
+		arr := this.value.([]*KeyValue)
+		for _, item := range arr {
+			key := item.key
+			if _, value := allKeys[key]; !value {
+				allKeys[key] = true
+				list = append(list, item)
+				item.RemoveDuplicates()
+			}
+		}
+		this.value = list
+	}
+}
+
+func (this KeyValue) Print(optional ...int) {
+	tabs := 0
+	if len(optional) > 0 {
+		tabs = optional[0]
+	}
+
+	if this.isRoot {
+		tabs = -1
+	}
+
+	switch this.value.(type) {
+	case []*KeyValue:
+		if !this.isRoot {
+			PrintTabs(tabs)
+			fmt.Println("\"" + this.key + "\"")
+			PrintTabs(tabs)
+			fmt.Println("{")
+		}
+		arr := this.value.([]*KeyValue)
+		for _, val := range arr {
+			val.Print(tabs + 1);
+		}
+
+		if !this.isRoot {
+			PrintTabs(tabs)
+			fmt.Println("}")
+		}
+	case string:
+		PrintTabs(tabs)
+		fmt.Println("\"" + this.key + "\"		\"" + this.value.(string) + "\"")
+	default:
+		fmt.Println(this)
+		panic("unknown type")
+	}
+}
+
+func (this *KeyValue) toJSON() interface{} {
+	ret := make(map[string]interface{})
+
+	switch this.value.(type) {
+	case string:
+		return this.value.(string)
+	case []*KeyValue:
+		arr := this.value.([]*KeyValue)
+		for _, kv := range arr {
+			ret[kv.key] = kv.toJSON()
+		}
+	}
+
+	return ret
+}
+
+func (this KeyValue) MarshalJSON() ([]byte, error) {
+	var ret interface{}
+
+	ret = this.toJSON()
+
+	return json.Marshal(ret)
+}
+
+
+func (this *VDF) Parse(s []byte) KeyValue {
 	this.s = s
 	this.i = 0
 	this.len = len(s)
-	result := map[string]interface{}{}
 
 	stringStack := stack.New()
 	levelStack := stack.New()
-	currentLevel := result
+
+	var currentLevel *KeyValue = &KeyValue{key: "root", value: []*KeyValue{}, isRoot: true}
+	var result KeyValue
 
 TokenLoop:
 	for {
@@ -40,27 +238,28 @@ TokenLoop:
 		switch token {
 		case openingBrace:
 			key := stringStack.Pop().(string)
-			var subLevel map[string]interface{}
-			if sl, exist := currentLevel[key]; exist {
-				subLevel = sl.(map[string]interface{})
-			} else {
-				subLevel = map[string]interface{}{}
+			subLevel := KeyValue{key: key, value: []*KeyValue{}}
+
+			if currentLevel != nil {
+				currentLevel.value = append(currentLevel.value.([]*KeyValue), &subLevel)
 			}
-			currentLevel[key] = subLevel
+
 			levelStack.Push(currentLevel)
-			currentLevel = subLevel
+			currentLevel = &subLevel
 		case closingBrace:
-			currentLevel = levelStack.Pop().(map[string]interface{})
+			currentLevel = levelStack.Pop().(*KeyValue)
+			if currentLevel != nil {
+				result = *currentLevel
+			}
 		case newLine:
 			if stringStack.Len() > 1 {
 				value := stringStack.Pop().(string)
 				key := stringStack.Pop().(string)
-				currentLevel[key] = value
+				currentLevel.value = append(currentLevel.value.([]*KeyValue), &KeyValue{key: key, value: value})
 			}
 		case stringValue: stringStack.Push(s)
 		case endToken: break TokenLoop
 		}
-
 	}
 
 	return result
